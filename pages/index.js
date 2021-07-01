6244b54559c23d6ee43c50ec1572d9dd9b41624c
Soojung Kwak <gsj2464@gmail.com>
2021-07-01 11:00:04 +0900

    body 추가

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,7 @@
 import Head from 'next/head';
+import Image from 'next/image';
+import { ViewGridIcon, MicrophoneIcon } from '@heroicons/react/solid';
+import { SearchIcon } from '@heroicons/react/outline';
 import Avatar from '../components/Avatar';
 
 export default function Home() {
@@ -10,27 +13,43 @@ export default function Home() {
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      {/* Header */}
       <header className='flex w-full p-5 justify-between text-sm text-gray-700'>
-        {/* Left */}
         <div className='flex space-x-4 items-center'>
           <p className='link'>About</p>
           <p className='link'>Store</p>
         </div>
 
-        {/* Right */}
         <div className='flex space-x-4 items-center'>
           <p className='link'>Gmail</p>
           <p className='link'>Images</p>
 
-          {/* Icon */}
+          <ViewGridIcon className='h-10 w-10 p-2 rounded-full hover:bg-gray-100 cursor-pointer' />
 
-          {/* Avatar */}
           <Avatar url='https://lh3.googleusercontent.com/ogw/ADea4I7fUwq5gjrt3CNl8mvmTtOB3jFyGhipeJDWORYtbw=s83-c-mo' />
         </div>
       </header>
 
-      {/* Body */}
+      <form className='flex flex-col items-center mt-44 flex-grow'>
+        <Image
+          src='https://www.google.co.kr/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png'
+          width={300}
+          height={100}
+        />
+
+        <div className='flex w-full mt-5 hover:shadow-lg focus-within:shadow-lg max-w-md rounded-full border border-gray-200 px-5 py-3 items-center sm:max-w-xl lg:max-w-2xl'>
+          <SearchIcon className='h-5 mr-3 text-gray-500' />
+
+          <input type='text' className='focus:outline-none flex-grow' />
+
+          <MicrophoneIcon className='h-5' />
+        </div>
+
+        <div className='flex flex-col w-1/2 space-y-2 justify-center mt-8 sm:space-y-0 sm:flex-row sm:space-x-4'>
+          <button className='btn'>Google Search</button>
+
+          <button className='btn'>I'm Feeling Lucky</button>
+        </div>
+      </form>
 
       {/* Footer */}
     </div>
