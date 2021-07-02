5119a16c645ab870f7c773fe98822887a716c0a4
Soojung Kwak <gsj2464@gmail.com>
2021-07-03 08:59:49 +0900

    search 기능 추가

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+import { useRef } from 'react';
+import { useRouter } from 'next/router';
 import Head from 'next/head';
 import Image from 'next/image';
 import { ViewGridIcon, MicrophoneIcon } from '@heroicons/react/solid';
@@ -6,6 +8,18 @@ import Avatar from '../components/Avatar';
 import Footer from '../components/Footer';
 
 export default function Home() {
+  const searchInputRef = useRef(null);
+  const router = useRouter();
+
+  const search = (e) => {
+    e.preventDefault();
+
+    const term = searchInputRef.current.value;
+    if (!term) return;
+
+    router.push(`/search?term=${term}`);
+  };
+
   return (
     <div className='flex flex-col items-center justify-center h-screen'>
       <Head>
@@ -40,15 +54,23 @@ export default function Home() {
         <div className='flex w-full mt-5 hover:shadow-lg focus-within:shadow-lg max-w-md rounded-full border border-gray-200 px-5 py-3 items-center sm:max-w-xl lg:max-w-2xl'>
           <SearchIcon className='h-5 mr-3 text-gray-500' />
 
-          <input type='text' className='focus:outline-none flex-grow' />
+          <input
+            ref={searchInputRef}
+            type='text'
+            className='focus:outline-none flex-grow'
+          />
 
           <MicrophoneIcon className='h-5' />
         </div>
 
         <div className='flex flex-col w-1/2 space-y-2 justify-center mt-8 sm:space-y-0 sm:flex-row sm:space-x-4'>
-          <button className='btn'>Google Search</button>
+          <button onClick={search} className='btn'>
+            Google Search
+          </button>
 
-          <button className='btn'>I'm Feeling Lucky</button>
+          <button onClick={search} className='btn'>
+            I'm Feeling Lucky
+          </button>
         </div>
       </form>
 
