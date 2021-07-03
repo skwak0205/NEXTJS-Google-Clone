df16e15adb210453e160f156eacfe2b27f0b23c8
Soojung Kwak <gsj2464@gmail.com>
2021-07-03 14:34:13 +0900

    오류 수정

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,6 +1,8 @@
+import Image from 'next/image';
+
 const Avatar = ({ className, url }) => {
   return (
-    <img
+    <Image
       className={`h-10 rounded-full cursor-pointer transition duration-150 transform hover:scale-110 ${className}`}
       loading='lazy'
       src={url}
--- pages/index.js
+++ pages/index.js
@@ -69,7 +69,7 @@ export default function Home() {
           </button>
 
           <button onClick={search} className='btn'>
-            I'm Feeling Lucky
+            I am Feeling Lucky
           </button>
         </div>
       </form>
