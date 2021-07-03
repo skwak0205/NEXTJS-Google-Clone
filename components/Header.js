a76b46a68d4a90c7d1e9b0492f176a7d9bd80763
Soojung Kwak <gsj2464@gmail.com>
2021-07-03 14:36:55 +0900

    bug fix

--- components/Header.js
+++ components/Header.js
@@ -34,6 +34,7 @@ const Header = () => {
             type='text'
             ref={searchInputRef}
             className='flex-grow w-full focus:outline-none'
+            defaultValue={router.query.term}
           />
 
           <XIcon
