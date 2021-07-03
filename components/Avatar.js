b27cd3b50e47e3287b7adec2d18d745c48b1145d
Soojung Kwak <gsj2464@gmail.com>
2021-07-03 09:44:20 +0900

    header component 추가

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,7 +1,7 @@
-const Avatar = ({ url }) => {
+const Avatar = ({ className, url }) => {
   return (
     <img
-      className='h-10 rounded-full cursor-pointer transition duration-150 transform hover:scale-110'
+      className={`h-10 rounded-full cursor-pointer transition duration-150 transform hover:scale-110 ${className}`}
       loading='lazy'
       src={url}
       alt='profile pic'
