0fc5f79c304e9460748104717e9d6f1bfc9c1937
Soojung Kwak <gsj2464@gmail.com>
2021-07-03 14:15:34 +0900

    search results 추가

---
+++ keys.js
@@ -0,0 +1,3 @@
+export const API_KEY = process.env.REACT_APP_API_KEY;
+
+export const CONTEXT_KEY = process.env.REACT_APP_CONTEXT_KEY;
--- pages/search.js
+++ pages/search.js
@@ -1,20 +1,42 @@
+import { useRouter } from 'next/router';
 import Head from 'next/head';
 import Header from '../components/Header';
+import SearchResults from '../components/SearchResults';
+import Response from '../Response';
+import { API_KEY, CONTEXT_KEY } from '../keys';
 
-const Search = () => {
+const Search = ({ results }) => {
+  const router = useRouter();
+
   return (
     <div>
       <Head>
-        <title>Search Results</title>
+        <title>{router.query.term} - Google Search</title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      {/* Header */}
       <Header />
 
-      {/* Search Result */}
+      <SearchResults results={results} />
     </div>
   );
 };
 
 export default Search;
+
+export async function getServerSideProps(context) {
+  const useDummyData = true;
+  const startIndex = context.query.start || '0';
+
+  const data = useDummyData
+    ? Response
+    : await fetch(
+        `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${context.query.term}&start=${startIndex}`
+      ).then((response) => response.json());
+
+  return {
+    props: {
+      results: data,
+    },
+  };
+}
